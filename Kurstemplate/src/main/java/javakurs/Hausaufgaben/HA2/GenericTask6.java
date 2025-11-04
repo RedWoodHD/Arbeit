2c4b0eced439f9215e68bf08841a1e51000a0fc5
Elias <eliasganske@outlook.com>
2025-11-04 16:28:17 +0100

    Stunde 4 Aufgaben und so.

--- Kurstemplate/src/main/java/javakurs/Hausaufgaben/HA2/GenericTask6.java
+++ Kurstemplate/src/main/java/javakurs/Hausaufgaben/HA2/GenericTask6.java
@@ -9,8 +9,8 @@ public class GenericTask6<TYPE extends GenericTask6.Animal & GenericTask6.Pet>
    {
       final GenericTask6<Cat> cat = new GenericTask6<>();
       final GenericTask6<Dog> dog = new GenericTask6<>();
-      final GenericTask6<Lion> lion = new GenericTask6<>(); //kein Pet
-      final GenericTask6<NonAnimal> nonAnimal = new GenericTask6<>(); //kein Animal
+//      final GenericTask6<Lion> lion = new GenericTask6<>(); //kein Pet
+//      final GenericTask6<NonAnimal> nonAnimal = new GenericTask6<>(); //kein Animal
    }
 
    static class Cat extends Animal implements Pet {}
